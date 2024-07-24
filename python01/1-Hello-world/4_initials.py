e026bea4e6dbf185ab91ff8ed5affe75c67b999c
jonathan <jonathan.lemari@gmail.com>
2024-07-24 03:32:56 +0200

    posting first batch of exercises done

---
+++ python01/1-Hello-world/4_initials.py
@@ -0,0 +1,9 @@
+# i am french
+
+print("JJJJJ   L")
+print("  J     L")
+print("  J     L")
+print("  J     L")
+print("J J     L")
+print("J J     L")
+print(" JJ     LLLLL")
---
+++ python01/1-Hello-world/5_letter.py
@@ -0,0 +1,6 @@
+print("On the 08.07.2024")
+print("I am feeling okay, i managed to fix and use my bike to go to the library. Trying to sort life.")
+print("in learning to code i want to be able to create program but also to automatize some hardware such like lights.")
+print("look at it with some effort and not giving up, ou have learned a lot, you are on the right path")
+print("🥲")
+
